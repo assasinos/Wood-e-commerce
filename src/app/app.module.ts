a776056bf30c1f4fc54eb156f39ebadb329ceefe
assasinos <kubasem198@gmail.com>
2023-12-29 22:37:58 +0100

    Make Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,56 @@
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css',
+})
+export class CarouselComponent implements AfterViewInit {
+
+
+
+  ngAfterViewInit(): void {
+
+
+
+    let defaultTransform = 0;
+    const goNext = () => {
+      defaultTransform = defaultTransform -  GetItemWidth();
+      if (Math.abs(defaultTransform) >= GetMaxScroll())
+        defaultTransform = 0;
+      this.sliderElement.nativeElement.style.transform = 'translateX(' + defaultTransform + 'px)';
+    }
+    this.nextElement.nativeElement.addEventListener('click', goNext);
+    const goPrev = () => {
+      if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+      else defaultTransform = defaultTransform +  GetItemWidth();
+      this.sliderElement.nativeElement.style.transform = 'translateX(' + defaultTransform + 'px)';
+    }
+    this.prevElement.nativeElement.addEventListener('click', goPrev);
+
+
+    const GetItemWidth = () => {
+      return this.sliderElement.nativeElement.firstElementChild.clientWidth +parseInt(window.getComputedStyle(this.sliderElement.nativeElement).gap.replace('px', ''));
+    }
+
+    const GetMaxScroll = () => {
+      //Check if screen is small 
+      if (window.innerWidth < 768) {
+        return this.sliderElement.nativeElement.scrollWidth;
+      }
+      else if(window.innerWidth < 1024) {
+      return this.sliderElement.nativeElement.scrollWidth - GetItemWidth();
+      }
+      return this.sliderElement.nativeElement.scrollWidth - GetItemWidth() *2;
+
+
+    }
+
+  }
+  @ViewChild('next') nextElement!: ElementRef;
+  @ViewChild('previous') prevElement!: ElementRef;
+  @ViewChild('slider') sliderElement!: ElementRef;
+
+  
+
+}
