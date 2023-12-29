ce0c4043532ab603fa47223286133f4f2b8da00e
assasinos <kubasem198@gmail.com>
2023-12-29 10:15:33 +0100

    Add Flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite/lib/esm/components';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-commerce-wood-client';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
